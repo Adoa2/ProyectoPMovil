60f18fe1214afc07075bd23b68f027d9bda8b8f7
adolf <adolf@ADOLFO>
2024-10-28 21:50:46 -0600

    Primer Commit

---
+++ ejemplodart.dart
@@ -0,0 +1,18 @@
+void main() {
+  /*
+   Comentario multilinea
+
+   //Este es tambien un comentario
+   */
+  print("hola dart");
+
+  //Variables
+
+  var x = "Hola dart!";
+  double y = 1;
+
+  x = x + "ok";
+  print(x);
+  y = y + 1;
+  print(y);
+}
